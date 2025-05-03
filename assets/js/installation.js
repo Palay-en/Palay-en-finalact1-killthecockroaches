d16023f68453c0f009a3fbea177055dc8eb22764
Palay-en <157661737+Palay-en@users.noreply.github.com>
2025-05-03 16:17:33 +0800

    Add files via upload

---
+++ assets/js/installation.js
@@ -0,0 +1,20 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  const installBtn = document.getElementById('installBtn');
+  if (installBtn) {
+    installBtn.hidden = false;
+
+    installBtn.addEventListener('click', async () => {
+      if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        console.log(`User response: ${outcome}`);
+        deferredPrompt = null;
+        installBtn.hidden = true;
+      }
+    });
+  }
+});
